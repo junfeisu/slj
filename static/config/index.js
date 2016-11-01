decb607399fbeb86290f5baec5d52ad8a62164b1
sjffly <1982764150@qq.com>
2016-11-01 16:22:00 +0800

    change the proxy

--- static/config/index.js
+++ static/config/index.js
@@ -23,7 +23,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
         '/api': {
-            target: "http://127.0.0.1:3000",
+            target: "http://127.0.0.1:8000",
             changeOrigin: true
         }
     },
